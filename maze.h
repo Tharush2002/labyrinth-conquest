e7e1c3ecac3e64b60b4807ac490bcf404f748643
Tharusha Gunerathne <tharusha1205@gmail.com>
2025-08-29 21:24:12 +0530

    added the main structs

--- maze.h
+++ maze.h
@@ -1,5 +1,40 @@
 #ifndef MAZE_H
 #define MAZE_H
 
+typedef struct{
+	int floor;
+	int block_width_num;
+	int block_length_num;
+}block;
+
+typedef struct{
+	int start_floor;
+	int start_block_width_num;
+	int start_block_length_num;	
+	int end_floor;
+	int block_cell_width_num;
+	int block_cell_length_num;
+}stair;
+
+typedef struct{
+	int start_floor;
+	int end_floor;
+	int block_width_num;
+	int block_length_num;
+}pole;
+
+typedef struct{
+	int floor;
+	int start_block_width_num;
+	int start_block_length_num;	
+	int end_block_width_num;
+	int end_block_length_num;
+}wall;
+
+typedef struct{
+	int floor;
+	int block_width_num;
+	int block_length_num;
+}goal;
 
 #endif
