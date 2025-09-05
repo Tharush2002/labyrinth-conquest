dff841f48db02e62f2e1b75419131bc905aba58b
Tharush2002 <tharusha1205@gmail.com>
2025-09-05 10:03:02 +0530

    added the seed initilization logic

--- main.c
+++ main.c
@@ -1,6 +1,23 @@
 #include "maze.h"
 
+void init_seed(const char *seed_file){
+	FILE *fp = fopen(seed_file, "r");
+	if(fp == NULL){
+		printf("Error opening seed.txt\n");
+		return;
+	}
+
+	int seed;
+	if(fscanf(fp, "%d", &seed) != 1){
+		printf("Error reading seed from seed.txt\n");
+	}else{
+		srand(seed);
+	}
+	fclose(fp);
+}
+
 int main(){
+	init_seed("./seed.txt");
 	init_game();	
 	return 0;
 }
