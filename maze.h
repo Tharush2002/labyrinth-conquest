313d193aa4fab558887e5fd34b819f3b01490f1c
Tharush2002 <tharusha1205@gmail.com>
2025-09-09 20:37:22 +0530

    refactor maze.h: enhance Bawana state management and player structure; add logging functions

--- maze.h
+++ maze.h
@@ -11,22 +11,33 @@
 #define LENGTH 25
 
 #define BAWANA_FOOD_POISONING_ROUNDS 3
+#define BAWANA_FOOD_POISONING_BONUS 0
 #define BAWANA_DISORIENTED_ROUNDS 4
 #define BAWANA_TRIGGERED_ROUNDS 3
 #define BAWANA_DISORIENTED_BONUS 50
 #define BAWANA_TRIGGERED_BONUS 50
 #define BAWANA_TRIGGERED_MULTIPLIER 2
 #define BAWANA_HAPPY_BONUS 200
+
+#define BAWANA_ENTRANCE_FLOOR 0
+#define BAWANA_ENTRANCE_WIDTH 9
+#define BAWANA_ENTRANCE_LENGTH 19
+
 #define INITIAL_MOVEMENT_POINTS 100
 
 #define MAX_STAIRS_FROM_SAME_CELL 2
 #define MAX_POLES_FROM_SAME_CELL 1
 
-typedef enum { DIR_NA=-1, NORTH, SOUTH, EAST, WEST } Direction;
-typedef enum { BAWANA_NA=-1, FOOD_POISONING, DISORIENTED, TRIGGERED, HAPPY, BONUS}BawanaState;
-typedef enum { CONSUMER_NA=-1, ZERO, COST, BONUS, MULTIPLIER } ConsumeType;
+#define PLAYER_COUNT 3
+
+#define HOW_MANY_ROUNDS_TO_CHANGE_STAIR_DIR 5
+
+typedef enum { DIR_NA, NORTH, EAST, SOUTH, WEST } Direction;
+typedef enum { BAWANA_NA, FOOD_POISONING, DISORIENTED, TRIGGERED, HAPPY, NORMAL}BawanaState;
+typedef enum { CONSUMER_NA, ZERO, COST, BONUS, MULTIPLIER } ConsumeType;
 typedef enum { STAIR, POLE, WALL, BK_NORMAL} BlockType;
 typedef enum { UNI_UP, UNI_DOWN, BI } StairDirection;
+typedef enum { A, B, C } PlayerID;
 
 typedef struct{
 	int floor;
@@ -69,16 +80,17 @@ typedef struct{
 }Bawana;
 
 typedef struct{
-	char player_name;
+	PlayerID id;
 	Block *current_block;
 	Direction direction;
 	int rem_points;
 	Bawana bawana_effect;
+	int player_rounds;
 }Player;
 
 typedef struct{
-	Player player;
-	int round;
+	Player player[PLAYER_COUNT];
+	int rounds;
 	int movement_dice;
 	Direction direction_dice;
 }Game;
@@ -95,15 +107,19 @@ extern Game game_state;
 
 void init_game();
 void init_maze();
-
-void init_bawana();
 void assign_consumables();
-void change_stair_direction();
-int move_piece(Block *current_block);
+void init_bawana();
+Block* go_to_starting_pos(Player *player);
+void go_to_bawana(Player *player);
+int move_piece(Player *current_player);
+Block* get_dest_block(Player *current_player, int *tot_cost, int*rem_mp);
 
 // HELPER FUNCTIONS
 
 int roll_dice();
+Direction get_direction(int direction_dice);
+void change_stair_direction();
+void shuffle_array(void *arr, size_t n, size_t elem_size);
 Direction get_direction(int direction_dice);
 int is_blocked_by_wall(int floor, int width_num, int length_num);
 int is_blocked_by_stair(int floor, int width_num, int length_num);
@@ -112,8 +128,8 @@ int is_in_the_playable_area(int floor, int width, int length);
 int is_in_bawana_area(int floor, int width, int length);
 int is_in_starting_area(int floor, int width, int length);
 
-int can_move_entirely(Block *current_block, int *cost);
-void set_destination_block(Block *block);
+void set_destination_block(Block *block, Player *player);
+void calc_mp_cost_and_rem(int *cost, int* rem_mp, Block *block);
 Block* closest_sp_destination(Stair *s[], Pole *p[],
                               int non_looping_s[], int non_looping_p[]);
 
@@ -130,5 +146,33 @@ void load_stairs(const char *stairs_file);
 void load_poles(const char *poles_file);
 void load_walls(const char *walls_file);
 int load_flag(const char *flag_file);
+void free_maze();
+char* direction_to_string(Direction dir);
+char* bawana_state_to_string(BawanaState state);
+char* player_id_to_string(PlayerID id);
+
+// LOGGING FUNCTIONS
+
+void log_cannot_move_from_starting_area(Player *player);
+void log_can_move_from_starting_area(Player *player);
+void log_in_maze_with_dir_dice(Player *player);
+void log_in_maze_without_dir_dice(Player *player);
+void log_is_blocked_by_wall(Player *player);
+void log_at_dest(Player *player);
+void log_deliver_to_bawana(Player *player);
+void log_when_food_poisoning_starts(Player *player);
+void log_when_food_poisoning_exists(Player *player);
+void log_when_food_poisoning_ends(Player *player);
+void log_when_disoriented_starts(Player *player);
+void log_when_disoriented_exists(Player *player);
+void log_when_disoriented_ends(Player *player);
+void log_when_triggered_starts(Player *player);
+void log_when_triggered_exists(Player *player);
+void log_when_happy(Player *player);
+void log_when_normal(Player *player);
+void log_land_on_stair(Player *player, Block *prev_block);
+void log_land_on_pole(Player *player, Block *prev_block);
+void log_on_win(Player *player);
+
 
 #endif
